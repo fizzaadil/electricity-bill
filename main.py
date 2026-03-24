7381f67e553474b0cc3d32d714bd2210623ab983
fizzaadil <fizzaadil3455@gmail.com>
2026-03-24 18:22:00 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+# Take input of number of units consumed froom the user
+units = int(input(" Please enter Number of Units you Consumed : "))
+
+# Check conditions of units consumed
+# Then calculate amount and surcharge accordingly
+# Surcharge is the tax value
+
+# Check for units less tan 50
+if(units < 50):
+    amount = units * 2.60
+    surcharge = 25
+
+# Check for units less than 100
+elif(units <= 100):
+    amount = 130 + ((units - 50) * 3.25)
+    surcharge = 35
+
+# Check for units less than or equal to 200
+elif(units <= 200):
+    amount = 130 + 162.50 + ((units - 100) * 5.26)
+    surcharge = 45
+
+# Check for all the cases other than mentioned
+# When units consumed are more than 200
+else:
+   amount = 130 + 162.50 + 526 ((units - 200) * 8.45)
+   surcharge = 75
+
+
+# Calculate and Display the totalelectricity bill
+# total amount = amount + surcharge
+total = amount + surcharge
+print("\nElectricity Bill = %.2f"  %total)
+
